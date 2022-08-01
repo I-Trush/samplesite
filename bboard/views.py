65090b2d4d5252f3ae29fe08abadba6e36477c54
I-Trush <gari-man@yandex.ru>
2022-08-01 21:43:03 +0300

    django template

--- bboard/views.py
+++ bboard/views.py
@@ -3,13 +3,18 @@
 # Create your views here.
 
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Bb
 
 
 def index(request):
-    # return HttpResponse("здесь будет выведен список объявлений")
-    s = 'Список объявлений\r\n\r\n\r\n'
-    for bb in Bb.objects.order_by('-published'):  # '-' означает в обратном порядке
-        s += str(bb.pk) + '\r\n' + bb.title + '\r\n' + bb.content + '\r\n\r\n'
-    return HttpResponse(s, content_type='text/plain; charset=utf-8')  # content_type без этого параметра веб-обозреватель посчитает текст HTML кодом
+    ## return HttpResponse("здесь будет выведен список объявлений")
+    #s = 'Список объявлений\r\n\r\n\r\n'
+    #for bb in Bb.objects.order_by('-published'):  # '-' означает в обратном порядке
+    #   s += str(bb.pk) + '\r\n' + bb.title + '\r\n' + bb.content + '\r\n\r\n'
+    #return HttpResponse(s, content_type='text/plain; charset=utf-8')  # content_type без этого параметра веб-обозреватель посчитает текст HTML кодом
+    template = loader.get_template('bboard/index.html')
+    bbs = Bb.objects.order_by('-published')
+    context = {'bbs':bbs}
+    return  HttpResponse(template.render(context, request))
