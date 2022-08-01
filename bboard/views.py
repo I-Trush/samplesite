6a7dd03cf1d4f8152ddde32f406c1981369d382e
I-Trush <gari-man@yandex.ru>
2022-08-01 23:16:27 +0300

    django admin

--- bboard/views.py
+++ bboard/views.py
@@ -2,19 +2,27 @@
 
 # Create your views here.
 
-from django.http import HttpResponse
-from django.template import loader
+##from django.http import HttpResponse
+##from django.template import loader
+
+from django.shortcuts import render
 
 from .models import Bb
 
 
+
 def index(request):
-    ## return HttpResponse("здесь будет выведен список объявлений")
-    #s = 'Список объявлений\r\n\r\n\r\n'
-    #for bb in Bb.objects.order_by('-published'):  # '-' означает в обратном порядке
-    #   s += str(bb.pk) + '\r\n' + bb.title + '\r\n' + bb.content + '\r\n\r\n'
-    #return HttpResponse(s, content_type='text/plain; charset=utf-8')  # content_type без этого параметра веб-обозреватель посчитает текст HTML кодом
-    template = loader.get_template('bboard/index.html')
+    # return HttpResponse("здесь будет выведен список объявлений")
+
+    ##s = 'Список объявлений\r\n\r\n\r\n'
+    ##for bb in Bb.objects.order_by('-published'):  # '-' означает в обратном порядке
+    ##   s += str(bb.pk) + '\r\n' + bb.title + '\r\n' + bb.content + '\r\n\r\n'
+    ##return HttpResponse(s, content_type='text/plain; charset=utf-8')  # content_type без этого параметра веб-обозреватель посчитает текст HTML кодом
+
+    ###template = loader.get_template('bboard/index.html')
+    ###bbs = Bb.objects.order_by('-published')
+    ###context = {'bbs':bbs}
+    ###return  HttpResponse(template.render(context, request))
+
     bbs = Bb.objects.order_by('-published')
-    context = {'bbs':bbs}
-    return  HttpResponse(template.render(context, request))
+    return render(request, 'bboard/index.html', {'bbs':bbs})
