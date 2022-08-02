6452f67200b187c622d3a0fd543ee53e160df9c3
I-Trush <gari-man@yandex.ru>
2022-08-03 02:30:55 +0300

    django admin model rubric connection

--- bboard/models.py
+++ bboard/models.py
@@ -8,8 +8,21 @@ class Bb(models.Model):
     price = models.FloatField(null=True, blank=True, verbose_name='Цена')
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Дата публикации')
 
-    class Meta:                             #класс, атрибуты которого зададут параметры самой додели
-        verbose_name_plural = 'Объявления'  #название модели во множественном числе
-        verbose_name = 'Объявление'         #название модели в единственном числе
-        ordering = ['-published']           #последовательность полей, для которых будет выполняться сортировка по умолчанию
+    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Рубрика')   # связь внешний ключ, теперь эта модель вторична, а модель Rubric первична//on_delete=models.PROTECT запрет каскадного удаления
 
+    class Meta:                             # класс, атрибуты которого зададут параметры самой додели
+        verbose_name_plural = 'Объявления'  # название модели во множественном числе
+        verbose_name = 'Объявление'         # название модели в единственном числе
+        ordering = ['-published']           # последовательность полей, для которых будет выполняться сортировка по умолчанию
+
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index=True, verbose_name='Название')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Рубрики'
+        verbose_name = 'Рубрика'
+        ordering = ['name']
--- bboard/views.py
+++ bboard/views.py
@@ -2,8 +2,8 @@
 
 # Create your views here.
 
-##from django.http import HttpResponse
-##from django.template import loader
+# #from django.http import HttpResponse
+# #from django.template import loader
 
 from django.shortcuts import render
 
@@ -14,15 +14,15 @@ from .models import Bb
 def index(request):
     # return HttpResponse("здесь будет выведен список объявлений")
 
-    ##s = 'Список объявлений\r\n\r\n\r\n'
-    ##for bb in Bb.objects.order_by('-published'):  # '-' означает в обратном порядке
-    ##   s += str(bb.pk) + '\r\n' + bb.title + '\r\n' + bb.content + '\r\n\r\n'
-    ##return HttpResponse(s, content_type='text/plain; charset=utf-8')  # content_type без этого параметра веб-обозреватель посчитает текст HTML кодом
+    # #s = 'Список объявлений\r\n\r\n\r\n'
+    # #for bb in Bb.objects.order_by('-published'):  # '-' означает в обратном порядке
+    # #   s += str(bb.pk) + '\r\n' + bb.title + '\r\n' + bb.content + '\r\n\r\n'
+    # #return HttpResponse(s, content_type='text/plain; charset=utf-8')  # content_type без этого параметра веб-обозреватель посчитает текст HTML кодом
 
-    ###template = loader.get_template('bboard/index.html')
-    ###bbs = Bb.objects.order_by('-published')
-    ###context = {'bbs':bbs}
-    ###return  HttpResponse(template.render(context, request))
+    # ##template = loader.get_template('bboard/index.html')
+    # ##bbs = Bb.objects.order_by('-published')
+    # ##context = {'bbs':bbs}
+    # ##return  HttpResponse(template.render(context, request))
 
     bbs = Bb.objects.all()
     return render(request, 'bboard/index.html', {'bbs':bbs})
