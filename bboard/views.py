81e55a7327a601525e2c6495b86247c3a7cd62c4
I-Trush <gari-man@yandex.ru>
2022-08-03 04:22:05 +0300

    django 2.3 url parameters

--- bboard/views.py
+++ bboard/views.py
@@ -8,7 +8,7 @@
 from django.shortcuts import render
 
 from .models import Bb
-
+from .models import Rubric
 
 
 def index(request):
@@ -19,10 +19,21 @@ def index(request):
     # #   s += str(bb.pk) + '\r\n' + bb.title + '\r\n' + bb.content + '\r\n\r\n'
     # #return HttpResponse(s, content_type='text/plain; charset=utf-8')  # content_type без этого параметра веб-обозреватель посчитает текст HTML кодом
 
-    # ##template = loader.get_template('bboard/index.html')
+    # ##template = loader.get_template('bboard/index.html')     #get_template загружает шаблон; параметр = путь
     # ##bbs = Bb.objects.order_by('-published')
     # ##context = {'bbs':bbs}
     # ##return  HttpResponse(template.render(context, request))
 
     bbs = Bb.objects.all()
-    return render(request, 'bboard/index.html', {'bbs':bbs})
+    rubrics = Rubric.objects.all()
+    context = {'bbs':bbs, 'rubrics':rubrics}                # набор параметров для вывода в шаблон index.html
+    return render(request, 'bboard/index.html', context)
+
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs':bbs, 'rubrics':rubrics, 'current_rubric':current_rubric}
+    return render(request, 'bboard/by_rubric.html', context)
+
